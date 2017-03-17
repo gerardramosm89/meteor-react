7a8f6d3690d091b8716fc6d565c3dabff314a596
gramosubuntu <gerard.ramosm89@gmail.com>
2017-03-17 14:40:44 -0700

    submitting to db from client

--- client/main.js
+++ client/main.js
@@ -4,40 +4,45 @@ import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 import { Tracker } from 'meteor/tracker';
 
-Tracker.autorun(() => {
-  console.log('players list', Players.find().fetch());
-});
-
-const players = [{
-  _id: '1',
-  name: "Gerry",
-  score: 99
-},{
-  _id: '2',
-  name: "Chris",
-  score: 2
-},{
-  _id: '3',
-  name: "Theresa",
-  score: 3
-}];
+// const players = [{
+//   _id: '1',
+//   name: "Gerry",
+//   score: 99
+// },{
+//   _id: '2',
+//   name: "Chris",
+//   score: 2
+// },{
+//   _id: '3',
+//   name: "Theresa",
+//   score: 3
+// }];
 
 const renderPlayers = (playersList) => {
   return playersList.map(player => {
-    return <p key={player.name}>{player.name} has {player.score} points.</p>
+    console.log(player);
+    return <p key={player._id}>{player.name} has {player.score} points.</p>
   });
 };
 
 Meteor.startup(function () {
   // title -> Account Settings
-  let name = 'gerry';
-  let jsx = (
-  <div>
-    <p>
-      This is from main.js {name}
-    </p>
-    { renderPlayers(players) }
-  </div>
-  );
-  ReactDOM.render(jsx, document.getElementById('app'));
+  Tracker.autorun(function() {
+    players = Players.find().fetch();
+    let jsx = (
+      <div>
+        <p>
+          This is from main.js {name}
+        </p>
+        { renderPlayers(players) }
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+  Players.insert({
+    name: "Gerry",
+    score: 23
+  });
+  // Insert new doc into players collection
+
 });
