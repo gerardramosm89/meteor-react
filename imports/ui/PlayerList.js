086306530abd42040b81ec9896ac49c8a8b1657b
gramosubuntu <gerard.ramosm89@gmail.com>
2017-03-22 16:56:24 -0700

    list

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import Player from './Player';
 import FlipMove from 'react-flip-move';
 import { Players, calculatePlayerPositions } from '../api/players';
-export default class PlayerList extends Component {
 
+
+export default class PlayerList extends Component {
   componentDidMount() {
-    Tracker.autorun(() => {
+    this.playersTracker = Tracker.autorun(() => {
       let players = Players.find({}, { sort: { score: -1 }}).fetch();
       let positionedPlayers = calculatePlayerPositions(players);
       console.log("positionedplayers from PlayerList", positionedPlayers.length);
+      this.setState({ players });
     });
   }
-  renderPlayer(players) {
-    if (!players) {
-      return <div>Loading</div>
-    }
-    if (players.length === 0) {
+
+  componentWillUnmount() {
+    this.playersTracker.stop();
+  }
+
+  renderPlayer() {
+    return <div>testwtfff</div>
+    /*if (!players) {
       return (
         <div className="item">
           <h2 className="item__message text-center">Please enter a player</h2>
@@ -25,7 +30,7 @@ export default class PlayerList extends Component {
       return players.map((player) => {
         return <Player key={player._id} player={player} />
       });
-    }
+    }*/
   }
   render (){
     return(
@@ -33,7 +38,7 @@ export default class PlayerList extends Component {
         <h1 className="text-center playerlist--header">Player List</h1>
         <FlipMove easing="cubic-bezier(0, 0.7, 0.5, 0.1)"
         maintainContainerHeight={true}>
-          {this.renderPlayer(this.props.players)}
+          {this.renderPlayer()}
         </FlipMove>
       </div>
     );
--- imports/ui/ScoreApp.js
+++ imports/ui/ScoreApp.js
@@ -5,24 +5,11 @@ import { Players, calculatePlayerPositions } from './../api/players';
 
 
 export default class ScoreApp extends Component {
-  componentWillMount() {
-    console.log("componentWillMount");
-    this.players = Players.find({}, { sort: { score: -1 }}).fetch();
-    console.log(this.players);
-  }
-  componentDidMount() {
-    console.log("componentDidMount");
-    setTimeout(function(){
-      console.log("timeout");
-      console.log(this.players);
-    }, 1000);
-  }
   render() {
-    console.log("This.props.players from ScoreApp is: ", this.props.players);
     return (
       <div>
         <div className="col-xs-6 col-xs-offset-3">
-          <PlayerList players={this.props.players} />
+          <PlayerList />
           <AddPlayer />
         </div>
       </div>
