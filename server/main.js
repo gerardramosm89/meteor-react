7d9c1de4ca89dfab7e0aaed1c968a1c3235cce23
Cudablogv2 <gerard.ramosm89@gmail.com>
2017-03-23 21:13:14 -0700

    change

--- server/main.js
+++ server/main.js
@@ -8,12 +8,9 @@ import '../imports/startup/simple-schema-configuration';
 Meteor.startup(() => {
 
   WebApp.connectHandlers.use((req, res, next) => {
-    console.log(req.url.slice(1));
     let _id = req.url.slice(1);
     let link = Links.findOne({ _id });
-    console.log(Links.findOne({ _id }));
     if (link) {
-      console.log("link is: ", link);
       res.statusCode = 302;
       res.setHeader('Location', link.url);
       res.end();
