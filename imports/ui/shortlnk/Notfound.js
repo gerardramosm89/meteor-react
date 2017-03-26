0b27d92bbe8b61f2f3a54390fca9818ee966de6c
Cudablogv2 <gerard.ramosm89@gmail.com>
2017-03-26 13:41:01 -0700

    variables

--- imports/ui/shortlnk/Notfound.js
+++ imports/ui/shortlnk/Notfound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 /*export default class NotFound extends React.Component {
   render() {
@@ -14,6 +15,7 @@ export default () => {
     return (
       <div>
         404 not found.
+        <Link to="/">Click to head home</Link>
       </div>
     );
 }
